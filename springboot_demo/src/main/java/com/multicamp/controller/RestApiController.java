ed75f160f5ef876f7f714d9130cbe408fb2fdd3c
swanbaek <swanbaek@naver.com>
2023-06-21 13:30:37 +0900

    RestApiController에 "/board/test" 매핑 추가하여 React와 연동할 준비함

--- springboot_demo/src/main/java/com/multicamp/controller/RestApiController.java
+++ springboot_demo/src/main/java/com/multicamp/controller/RestApiController.java
@@ -1,10 +1,25 @@
 package com.multicamp.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@RequestMapping("/board")
 public class RestApiController {
-	
-	
+	@ApiOperation(value = "board springboot react test", produces = "application/json")
+	@GetMapping(value="/test",  produces = "application/json")
+	public Map<String, Object> test() {
+		Map<String, Object> res = new HashMap<>();
+        res.put("success", true);
+        res.put("msg", "Welcome to SpringBoot & React App");
+        
+        return res;
+	}
 
 }
