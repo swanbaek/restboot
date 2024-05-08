f14bcfe8ffb402689aba1bf8d55cb5d5515d1798
DESKTOP-T5B94PA\swanb <swanbaek@naver.com>
2024-05-08 10:19:14 +0900

    @Builder 어노테이션 추가

--- springboot_demo/src/main/java/com/multicamp/domain/UserVO.java
+++ springboot_demo/src/main/java/com/multicamp/domain/UserVO.java
@@ -1,15 +1,10 @@
 package com.multicamp.domain;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserVO  
 {
 	
